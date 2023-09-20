bf8d0efd7b3c20cd9b8aae164754bf12b3bacd2b
ElArjun-Dev <arjundev.contact@gmail.com>
2023-09-20 15:24:54 +0200

    crud

--- src/Controller/AdminController.php
+++ src/Controller/AdminController.php
@@ -103,4 +103,48 @@ class AdminController extends AbstractController
             'form' => $form,
         ]);
     }
+
+    //Méthode pour modifier un jeu
+    #[Route('/edit/{id}', name: 'app_game_edit', methods: ['GET', 'POST'])]
+    public function edit(Request $request,Game $game, GameRepository $gameRepository)
+    {
+        $form = $this->createForm(GameType::class, $game);
+        $form->handleRequest($request);
+
+        if($form->isSubmitted() && $form->isValid())
+        {
+
+            // gestion de l'image uploadée
+            $imageFile = $form->get('image')->getData();
+            if($imageFile)
+            {
+                //si une image est uploadée, on récupere son nom d'origine
+                $originalFilename = pathinfo($imageFile->getClientOriginalName(), PATHINFO_FILENAME);
+                //on génere un nom unique pour l'image pour evité decraser des images ayant le meme nom
+                $newFilename = $originalFilename . '-' . uniqid() . '.' . $imageFile->guessExtension();
+                Try{
+                    //on déplace l'image uploadée dans le dossier public/images
+                    $imageFile->move(
+                        //games_images_directory est configuré dans config/services.yaml
+                        $this->getParameter('game_images_directory'),
+                        $newFilename
+                    );
+                }catch(FileException $e)
+                {
+                    $this->addFlash('danger', 'Une erreur est survenue lors de l\'upload de l\'images');
+                }
+
+                // on donne le nouveau nom pour la bdd
+                $game->setImagePath($newFilename);
+            }
+            
+            $gameRepository->save($game, true);
+            return $this->redirectToRoute('app_admin', [], Response::HTTP_SEE_OTHER);
+        }
+
+        return $this->renderForm('game/edit.html.twig', [
+            'game' => $game,
+            'form' => $form
+        ]);
+    }
 }
